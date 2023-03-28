c0ee258e50cf1b164894b75cdaf6090d0e1ab73b
RuslanaSemenenko <113388430+RuslanaSemenenko@users.noreply.github.com>
2023-03-28 22:52:02 +0100

    js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import './css/styles.css';
-import './css/styles.css';
 import { fetchCountries } from './js/fetchCountries';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
